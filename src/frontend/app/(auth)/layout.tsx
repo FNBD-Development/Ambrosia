b2e552f450036308156504af903368b0884d7e2e
eveeify <eveeg1971@gmail.com>
2023-12-22 02:26:02 +1100

    init.

---
+++ src/frontend/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-red-400 min-h-full w-full">{children}</div>;
+};
+
+export default AuthLayout;
---
+++ src/frontend/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./global.css";
+
+const font = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ambrosia | Server Management",
+  description: "Cool xD",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={(font.className, "bg-grey-400 h-full w-full")}>
+        {children}
+      </body>
+    </html>
+  );
+}
